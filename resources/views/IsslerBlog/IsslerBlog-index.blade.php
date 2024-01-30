a0a095f71c6ddcdddf57353c931b89901f3608c5
issler78 <isslermatheus0@gmail.com>
2024-01-30 20:30:54 -0300

    feat: create main layout and using in the index view

--- resources/views/IsslerBlog/IsslerBlog-index.blade.php
+++ resources/views/IsslerBlog/IsslerBlog-index.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IsslerBlog</title>
-</head>
-<body>
+@extends('IsslerBlog.Layouts.IsslerBlogMain')
+
+@section('content')
     <h1>IsslerBlog</h1>
-</body>
-</html>
+@endsection

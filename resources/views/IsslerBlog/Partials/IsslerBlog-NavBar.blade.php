002e5f35164547d4a6795f4f6798f691c1508826
issler78 <isslermatheus0@gmail.com>
2024-02-01 23:31:20 -0300

    feat: create navbar

---
+++ resources/views/IsslerBlog/Partials/IsslerBlog-NavBar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center gap-2" href="/IsslerBlog"><img src="https://cdn-icons-png.flaticon.com/512/6930/6930122.png" alt="IsslerBlog Icon" width="30" height="30">IsslerBlog</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/IsslerBlog"><i class="bi bi-house-fill"></i> Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Front-End</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Back-End</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Mobile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Guias</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <div class="input-group">
+                    <input type="text" class="form-control rounded-start-pill" placeholder="Search" aria-label="Search" aria-describedby="Search-Icon">
+                    <span class="input-group-text rounded-end-pill" id="Search-Icon">
+                        <i class="bi bi-search"></i>
+                    </span>
+                </div>
+            </form>
+        </div>
+    </div>
+</nav>

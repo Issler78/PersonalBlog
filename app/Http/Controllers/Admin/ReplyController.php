6958dff95ceaf5496740f15b3c2ed8b639e15b3e
issler78 <isslermatheus0@gmail.com>
2024-03-07 22:38:34 -0300

    feat: create reply service and define in controller

--- app/Http/Controllers/Admin/ReplyController.php
+++ app/Http/Controllers/Admin/ReplyController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdateReplyRequest;
+use App\Services\ReplyService;
 
 class ReplyController extends Controller
 {
+    public function __construct(
+        protected ReplyService $service
+    ){}
+
     public function store(StoreUpdateReplyRequest $request)
     {
-        dd($request);
+        $this->service->new();
     }
 }
---
+++ app/Services/ReplyService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class ReplyService
+{
+    public function new()
+    {
+        dd('new');
+    }
+}

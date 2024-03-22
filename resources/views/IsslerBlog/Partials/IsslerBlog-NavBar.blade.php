a60825b41755e56ce5f60066c33b9e1269a93d8c
issler78 <isslermatheus0@gmail.com>
2024-03-22 20:34:39 -0300

    feat: create Sign In navbar link

--- resources/views/IsslerBlog/Partials/IsslerBlog-NavBar.blade.php
+++ resources/views/IsslerBlog/Partials/IsslerBlog-NavBar.blade.php
@@ -7,7 +7,7 @@
     box-shadow: 0 2px #fff;
 }
 </style>
-<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark border-bottom border-body" data-bs-theme="dark">
+<nav class="navbar navbar-expand-xl sticky-top navbar-dark bg-dark border-bottom border-body" data-bs-theme="dark">
     <div class="container-fluid">
         <a class="navbar-brand d-flex align-items-center gap-2" href="{{ route('IsslerBlog.index') }}"><img src="https://cdn-icons-png.flaticon.com/512/6930/6930122.png" alt="IsslerBlog Icon" width="30" height="30">IsslerBlog</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,7 +16,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('IsslerBlog.index') }}"><i class="bi bi-house-fill"></i> Home</a>
+                    <a class="nav-link active" href="{{ route('IsslerBlog.index') }}"><i class="bi bi-house-fill"></i> Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('IsslerBlog.category', ['category' => 'F']) }}">Front-End</a>
@@ -29,6 +29,9 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('IsslerBlog.category', ['category' => 'G']) }}">Guides</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#"><i class="bi bi-person-fill"></i> Sign In</a>
                 </li>
             </ul>
             <form class="d-flex" action="{{ route('IsslerBlog.index') }}" method="GET">

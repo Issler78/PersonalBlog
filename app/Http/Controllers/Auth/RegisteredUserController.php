a23c7b50bc94901189a75036ffb47100951f957f
issler78 <isslermatheus0@gmail.com>
2024-03-28 21:37:10 -0300

    make store function to Register user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisteredUserController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-        dd('test');
+        $request->validate([
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|min:8'
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('IsslerBlog.index');
     }
 }

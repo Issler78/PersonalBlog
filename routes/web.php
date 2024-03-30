64aaeb0c997326873226d7ec2b7b38335fe1b942
issler78 <isslermatheus0@gmail.com>
2024-03-30 20:26:08 -0300

    create and use middleware to check if are an admin

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,19 @@ Route::get('/IsslerBlog/authenticate', function () {
 })->name('IsslerBlog.authenticate');
 
 // Posts
-Route::get('/IsslerBlog', [PostController::class, 'index'])->name('IsslerBlog.index');
-Route::get('/IsslerBlog/publish', [PostController::class, 'create'])->name('IsslerBlog.publish');
-Route::post('/IsslerBlog/publish', [PostController::class, 'store'])->name('IsslerBlog.store');
-Route::get('/IsslerBlog/{id}', [PostController::class, 'show'])->name('IsslerBlog.show');
-Route::get('/IsslerBlog/{id}/edit', [PostController::class, 'edit'])->name('IsslerBlog.edit');
-Route::put('/IsslerBlog/{id}/edit', [PostController::class, 'update'])->name('IsslerBlog.update');
-Route::delete('/IsslerBlog/{id}', [PostController::class, 'destroy'])->name('IsslerBlog.destroy');
+Route::middleware('auth.admin')->group(function () {
+    Route::get('/IsslerBlog/publish', [PostController::class, 'create'])->name('IsslerBlog.publish');
+    Route::post('/IsslerBlog/publish', [PostController::class, 'store'])->name('IsslerBlog.store');
+    Route::get('/IsslerBlog/{id}/edit', [PostController::class, 'edit'])->name('IsslerBlog.edit');
+    Route::put('/IsslerBlog/{id}/edit', [PostController::class, 'update'])->name('IsslerBlog.update');
+    Route::delete('/IsslerBlog/{id}', [PostController::class, 'destroy'])->name('IsslerBlog.destroy');
+});
 
+Route::get('/IsslerBlog', [PostController::class, 'index'])->name('IsslerBlog.index');
+Route::get('/IsslerBlog/{id}', [PostController::class, 'show'])->name('IsslerBlog.show');
 Route::get('/IsslerBlog/{category}/category', [PostController::class, 'index'])->name('IsslerBlog.category');
 
+// Reply
 Route::middleware('auth')->group(function () {
     Route::post('/IsslerBlog/publish/reply', [ReplyController::class, 'store'])->name('IsslerBlog.reply.publish');
     Route::delete('/IsslerBlog/{id}/reply', [ReplyController::class, 'destroy'])->name('IsslerBlog.reply.destroy');
